42439d4a8dc4c2b19784276c2e016ddd395fc33f
Xiaming Chen <chenxm35@gmail.com>
2016-10-25 23:52:59 +0800

    Update

--- utilities/ubuntu16-theme-MacBuntu.sh
+++ utilities/ubuntu16-theme-MacBuntu.sh
@@ -9,8 +9,12 @@ sudo apt-get update
 sudo apt-get install macbuntu-os-icons-lts-v7
 sudo apt-get install macbuntu-os-ithemes-lts-v7
 
+# Uninstall
+# cd /usr/share/icons/mac-cursors && sudo ./uninstall-mac-cursors.sh
+# sudo apt-get remove macbuntu-os-icons-lts-v7 macbuntu-os-ithemes-lts-v7
+
 # Slingscold (Alternative to Launchpad)
-sudo apt-get install slingscold
+# sudo apt-get install slingscold
 
 # Albert Spotlight (Alternative to Mac Spotlight)
 sudo apt-get install albert
@@ -20,15 +24,21 @@ sudo apt-get install plank
 
 # Tweak Tools to change Themes & Icons
 sudo apt-get install unity-tweak-tool
-sudo apt-get install gnome-tweak-tool
+# sudo apt-get install gnome-tweak-tool
 
 # Install Monochrome icons for Libreoffice:
 sudo apt-get install libreoffice-style-sifr
 
 # (Optional) Mac fonts:
 wget -O mac-fonts.zip http://drive.noobslab.com/data/Mac/macfonts.zip
-sudo unzip mac-fonts.zip -d /usr/share/fonts; rm mac-fonts.zip
+mkdir -p ~/.fonts/mac
+unzip mac-fonts.zip -d ~/.fonts/mac/; rm mac-fonts.zip
 sudo fc-cache -f -v
 
 # Apply MacBuntu Boot-screen/Splash:
 # sudo apt-get install macbuntu-os-bscreen-lts-v7
+
+# Default font of Mac OS
+# Console: Monaco
+# UI: Lucida Grande
+
--- utilities/ubuntu16_bundle.sh
+++ utilities/ubuntu16_bundle.sh
@@ -1,53 +1,43 @@
 #!/bin/bash
 # set -e
 
-# Add third-party ppas
+# Useful PPAs
 sudo apt-add-repository -y ppa:webupd8team/java
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 sudo add-apt-repository -y ppa:smathot/cogscinl
+sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
+sudo apt-add-repository -y ppa:mmk2410/intellij-idea-community
+
+# Install sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 
 # update yum repo
 sudo apt-get update
 
 # basics
-sudo apt-get install -y sublime-text-installer tmux vim zsh bash-completion emacs tree wget curl sshpass nano mlocate virtualbox gparted exfat-utils openvpn indicator-multiload openssh-server openssh-client python-pip sysv-rc-conf fcitx
+sudo apt-get install -y tmux vim zsh bash-completion emacs tree wget curl sshpass nano mlocate virtualbox gparted exfat-utils openvpn indicator-multiload openssh-server openssh-client python-pip sysv-rc-conf fcitx mpv smplayer zotero-standalone
 
 # system
 sudo apt-get install -y ntp lvm2 lshw usbutils pciutils denyhosts autofs mdadm iperf lshw sysstat fio iotop iftop htop iptraf tcpdump mtr tcpdump clusterssh pdsh nmap
 
 # development
-sudo apt-get install -y build-essential subversion r-base r-base-dev r-base-core gnuplot git git-svn git-cvs ant libcurl3 libcurl4-openssl-dev openssl libxml2-dev libxslt-dev maven libssl-dev exuberant-ctags
+sudo apt-get install -y build-essential subversion r-base r-base-dev r-base-core gnuplot git git-svn git-cvs ant libcurl3 libcurl4-openssl-dev openssl libxml2-dev libxslt-dev maven libssl-dev exuberant-ctags sbt sublime-text-installer ubuntu-make intellij-idea-community
 
 sudo pip install -U dbgp vim-debug pep8 flake8 pyflakes isort
 
 # Java and Scala
 sudo apt-get remove -y openjdk-*-jre-*
 sudo apt-get purge -y openjdk*
-sudo apt-get install -y oracle-java7-installer oracle-java7-set-default scala
+sudo apt-get install -y oracle-java8-installer oracle-java8-set-default scala
 
-# Sogou input and WPS
-curl http://cdn2.ime.sogou.com/dl/index/1465191139/sogoupinyin_2.0.0.0078_amd64.deb?st=hBYrFcS3EvCOkTfsqNK3cA&e=1465736249&fn=sogoupinyin_2.0.0.0078_amd64.deb -o sogoupinyin_2.0.0.0078_amd64.deb
-sudo apt-get install -y sogoupinyin_2.0.0.0078_amd64.deb
-sudo apt-get update
 # WPS in ubuntukylin repo
 # Fix GPG key error:
 # sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3C962022012520A0
 # replace final key with yours
-sudo apt-get install wps-office
-
-# Zotero
-sudo apt-get install zotero-standalone
-
-# Media players
-sudo apt-get install mpv smplayer
+# sudo apt-get install wps-office
 
 sudo apt-get autoremove
-
-# Install sbt
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install sbt
 
 # TMUX
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
