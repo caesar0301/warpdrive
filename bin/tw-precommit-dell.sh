07ad2cff7e0f24e39255b58f884163e466d41747
Xiaming Chen <chenxm35@gmail.com>
2016-12-05 16:17:09 +0800

    Update

--- bin/tw-precommit-dell.sh
+++ bin/tw-precommit-dell.sh
@@ -12,22 +12,22 @@ echo $THIS_HOME
 # TPC-DS
 echo "Starting TPC-DS ..."
 cd /home/benchmark/tpcds-2g-test
-./tpcds-test-2g.pl -c > $THIS_HOME/tpcds.log &
+./tpcds-test-2g.pl -c > $THIS_HOME/tpcds.log
 
 # TPC-H, Ignore 17的错误，是因为精度问题
 echo "Starting TPC-H ..."
 cd /home/zqchen/tpch_2_17_0/
-./runAll.sh tpch_orc_1 > $THIS_HOME/tpch.log &
+./runAll.sh tpch_orc_1 > $THIS_HOME/tpch.log
 
 # UT Data Type, Ignore 后面有几个timestamp相关的fail
 echo "Starting UT ..."
 cd /home/zqchen/jars
-./UT-datatype.sh > $THIS_HOME/ut.log &
+./UT-datatype.sh > $THIS_HOME/ut.log
 
 # CRUD, Ignore中间有个hbase相关的错误
 echo "Starting CRUD ..."
 cd /home/zqchen/jars/UT
-./runOrcCrud.sh > $THIS_HOME/crud.log &
+./runOrcCrud.sh > $THIS_HOME/crud.log
 
 echo "All job started!"
 
