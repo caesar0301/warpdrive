f26d6e19419816edb4652d2af468908a21f8361d
Xiaming Chen <chen_xm@outlook.com>
2016-12-13 11:52:18 +0800

    Update system.sh

--- learn_shell/system.sh
+++ learn_shell/system.sh
@@ -14,3 +14,11 @@ if [ $? -eq 0 ]; then
 else
   echo "Process is not running."
 fi
+
+# free memory
+# To free pagecache:
+echo 1 > /proc/sys/vm/drop_caches
+# To free dentries and inodes:
+echo 2 > /proc/sys/vm/drop_caches
+# To free pagecache, dentries and inodes:
+echo 3 > /proc/sys/vm/drop_caches
