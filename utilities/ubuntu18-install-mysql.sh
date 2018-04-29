338ce4e77caffb8ef7f605644db60ecd4a608247
Xiaming Chen <chenxm35@gmail.com>
2018-04-29 22:17:50 +0800

    Update ubuntu18-install-mysql.sh

--- utilities/ubuntu18-install-mysql.sh
+++ utilities/ubuntu18-install-mysql.sh
@@ -1,1 +1,10 @@
+sudo apt-get purge mysql-server
+sudo apt-get purge mysql-common
+sudo rm -rf /var/log/mysql
+sudo rm -rf /var/log/mysql.*
+sudo rm -rf /var/lib/mysql
+sudo rm -rf /etc/mysql
 
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+sudo apt-get install -y mysql-server mysql-common --fix-missing --fix-broken
