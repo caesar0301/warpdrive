f5ad8901beedbc4f9b88ef0c955367a3bc7d2520
Xiaming <chenxm35@gmail.com>
2018-07-16 12:17:51 +0800

    Create install.sh

---
+++ dotfiles/badass_terminal/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install tmux
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+cd /tmp && git clone https://github.com/powerline/fonts.git &&  sh fonts/install.sh && cd -
