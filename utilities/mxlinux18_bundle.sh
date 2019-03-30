05b1506b30b0f41630518f43446f43f4275f4adb
Xiaming <chenxm35@gmail.com>
2019-03-31 00:30:06 +0800

    Create mxlinux18_bundle.sh

---
+++ utilities/mxlinux18_bundle.sh
@@ -0,0 +1,5 @@
+# Install nodejs
+curl -sL https://deb.nodesource.com/setup_11.x > ./setup_11.x
+# vi setup_11.x and add:
+# check_alt "MX"            "Continuum" "Debian" "stretch"
+# check_alt "mx-linux"      "Continuum" "Debian" "stretch"
