f955a538219c94d6149362e88e6b1027eb8f5d59
Xiaming <chenxm35@gmail.com>
2019-10-01 23:52:33 +0800

    Update install_py.sh

--- utilities/install_py.sh
+++ utilities/install_py.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Install python dependencies: https://github.com/pyenv/pyenv/wiki
+# For ubuntu:
+# sudo apt-get install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 # Install a specified version of Python.
 # E.g., install_py.sh 3.6.5
 VERSION=$1
