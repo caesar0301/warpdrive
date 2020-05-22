d43114adc6c84b0e01dbc090e988216339f3376a
Xiaming <chenxm35@gmail.com>
2020-05-23 00:40:38 +0800

    Update mac_bundle.sh

--- bundles/mac_bundle.sh
+++ bundles/mac_bundle.sh
@@ -1,2 +1,3 @@
 brew extract bazel bazelbuild/tap --version 3.0.0
 brew install /usr/local/Homebrew/Library/Taps/bazelbuild/homebrew-tap/Formula/bazel@3.0.0.rb
+# Then refer to https://benbenbang.github.io/share/How-to-install-specific-formula-using-homebrew/
