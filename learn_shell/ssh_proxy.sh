9d9a82c680e3f4047abb94c82f6ed7fe7a29be2a
Xiaming <chenxm35@gmail.com>
2020-06-16 02:06:14 +0800

    Update ssh_proxy.sh

--- learn_shell/ssh_proxy.sh
+++ learn_shell/ssh_proxy.sh
@@ -12,3 +12,21 @@ ssh -o ProxyCommand="gpg -d -q ~/.ssh/.sshpass.gpg > /tmp/fifo; sshpass -f /tmp/
 
 # gpg without password promt
 echo gpg_password | gpg --batch --yes --passphrase-fd 0 your_file.gpg
+
+# with .ssh/config, https://serverfault.com/a/701884
+Host sw_jump
+  Hostname 61.164.33.194
+  Port 3700
+  User root
+  IdentityFile ~/.ssh/id_rsa
+  ControlPath ~/.ssh/controlmasters/%r@%h:%p
+  ControlMaster auto
+
+Host sw_huigui
+  Hostname vm010008014096
+  User admin
+  Port 22
+  IdentityFile ~/.ssh/id_rsa
+  ForwardAgent yes
+  # ProxyJump sw_jump
+  ProxyCommand ssh sw_jump 'ssh-add && nc %h %p'
