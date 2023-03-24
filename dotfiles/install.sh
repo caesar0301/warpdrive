b145f574b629f2c0bc73434fa9f68cba621b9cfa
xiaming.cxm <xiaming.cxm@alibaba-inc.com>
2023-03-24 11:49:05 +0800

    Update

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -6,23 +6,25 @@ thispath=$(dirname $abspath)
 SOFTLINK=1
 
 function install_zsh() {
-    echo "Configuring zsh..."
     if [ -e $HOME/.zi ]; then
-        echo "~/.zi existed, skip"
+        echo "WARNING: $HOME/.zi existed, skip"
     else
-        mkdir $HOME/.zi
-        git clone https://github.com/z-shell/zi.git ~/.zi/bin
+        typeset -Ag ZI
+        typeset -gx ZI[HOME_DIR]="${HOME}/.zi" ZI[BIN_DIR]="${ZI[HOME_DIR]}/bin"
+        command mkdir -p "$ZI[BIN_DIR]"
+        compaudit | xargs chown -R "$(whoami)" "$ZI[HOME_DIR]"
+        compaudit | xargs chmod -R go-w "$ZI[HOME_DIR]"
+        command git clone https://github.com/z-shell/zi.git "$ZI[BIN_DIR]"
     fi
     if [ x$SOFTLINK == "x1" ]; then
         ln -sf $thispath/zsh/zshrc $HOME/.zshrc
     else
         cp $thispath/zsh/zshrc $HOME/.zshrc
     fi
-    echo "Zsh configured"
+    echo "INFO: zsh configured"
 }
 
 function install_vim() {
-    echo "Configuring vim..."
     mkdir -p $HOME/.config/nvim
     cp $thispath/vim/nvim/init.vim $HOME/.config/nvim/init.vim
     mkdir -p $HOME/.vim/autoload
@@ -34,14 +36,13 @@ function install_vim() {
     else
         cp $thispath/vim/vimrc $HOME/.vimrc
     fi
-    echo "Vim configured, run :PlugInstall to install plugins"
+    echo "INFO: vim configured, run :PlugInstall to install plugins"
 }
 
 function install_tmux() {
-    echo "Configuring tmux..."
     mkdir -p $HOME/.tmux/plugins
     if [ -e $HOME/.tmux/plugins/tpm ]; then
-        echo "$HOME/.tmux/plugins/tpm existed, skip"
+        echo "WARNING: $HOME/.tmux/plugins/tpm existed, skip"
     else
         git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
     fi
@@ -50,13 +51,12 @@ function install_tmux() {
     else
         cp $thispath/tmux/tmux.conf $HOME/.tmux.conf
     fi
-    echo "Tmux configured, run <prefix>I to install plugins"
+    echo "INFO: tmux configured, run <prefix>I to install plugins"
 }
 
 function install_emacs() {
-    echo "Configuring emacs..."
     if [ -e $HOME/.emacs.d ]; then
-        echo "~/.emacs.d existed, skip"
+        echo "WARNING: $HOME/.emacs.d existed, skip"
     else
         mkdir ~/.emacs.d
     fi
@@ -70,7 +70,7 @@ function install_emacs() {
         cp -r $thispath/emacs/.emacs.d/settings $HOME/.emacs.d/
         cp $thispath/emacs/.emacs.d/init.el $HOME/.emacs.d/
     fi
-    echo "Emacs configured"
+    echo "INFO: emacs configured"
 }
 
 install_zsh
