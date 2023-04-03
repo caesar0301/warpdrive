3fadcd502881ae00817bc606a72043d808274778
chenxm <chenxm35@gmail.com>
2023-04-03 15:19:02 +0800

    Move to cool-dotfiles

---
+++ dotfiles/README.md
@@ -0,0 +1,1 @@
+Stuff under this folder was permanently moved to https://github.com/caesar0301/cool-dotfiles
--- dotfiles/emacs/.emacs.d/settings/helm-settings.el
+++
@@ -1,9 +0,0 @@
-(require 'helm)
-(require 'helm-descbinds)
-
-(fset 'describe-bindings 'helm-descbinds)
-(helm-mode 1)
-
-(global-set-key (kbd "C-c h") 'helm-mini)
-
-(provide 'helm-settings)
