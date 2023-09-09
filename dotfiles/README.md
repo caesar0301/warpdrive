779b6fcdd3667e1db87dd67d14c6ade03f081b39
xiaming.chen <chenxm35@gmail.com>
2023-09-09 11:59:19 +0800

    clean folder

--- dotfiles/README.md
+++
@@ -1,1 +0,0 @@
-Stuff under this folder was permanently moved to https://github.com/caesar0301/cool-dotfiles
--- utilities/findip4mac.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo 'Looking for IP addr ...'
-
-sudo nmap -sP 172.16.0.1/22 | awk '/Nmap scan report for/{printf $5;}/MAC Address:/{print " => "$3;}' | sort
-
-echo "Done!"
-exit 0
