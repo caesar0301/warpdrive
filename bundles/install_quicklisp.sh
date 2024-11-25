fd5c64e1a0737f325f80003b435800f0250972ac
Xiaming Chen <chenxm35@gmail.com>
2024-11-25 23:11:40 +0800

    Create install_quicklisp.sh

---
+++ bundles/install_quicklisp.sh
@@ -0,0 +1,5 @@
+curl -o /tmp/ql.lisp http://beta.quicklisp.org/quicklisp.lisp
+sbcl --no-sysinit --no-userinit --load /tmp/ql.lisp \
+       --eval '(quicklisp-quickstart:install :path "~/.quicklisp")' \
+       --eval '(ql:add-to-init-file)' \
+       --quit
